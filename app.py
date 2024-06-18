e0d68522a61995c79f14d7eaba59a5cd32e7165f
Yan Zhang <yanyzhang1@gmail.com>
2024-06-18 16:39:19 -0400

    updated subscribe POST, still to fix the posting

--- app.py
+++ app.py
@@ -1,32 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
+
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:{password}@localhost:5432/mtasubwaydelays'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:groe46disc@localhost:5432/mtasubwaydelays'
 # Debugging line to print the URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
 
 
+
+class EmailData(db.Model):
+    __tablename__ = "email_list"
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.SmallInteger, nullable=False)
+
+    def __init__(self, email):
+        self.email=email
+
+
 with app.app_context():
     db.create_all()
+
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-# @app.route("/thankyou", method=['POST'])
-# def thankyou():
-#     if request.method=='POST':
-#         email=request.form['email_name']
-
-#         return render_template('thankyou.html')
 
 
 
+@app.route("/subscript", methods=['POST'])
+def subscript():
+    if request.method=='POST':
+        email=request.form['email_name']
+        print(request.form)
 
+        subscriptionlist=EmailData(email)
+        db.session.add(subscriptionlist)
+        db.session.commit()
 
+        flash("Thank you for subscribing to MTA Subway Delays!")
+        return render_template('index.html')
 
 
 
